08be9555a36392a720255a3f45cdb38fa7cb6732
xuzhen <zhenx@zju.edu.cn>
2024-01-20 16:20:09 +0800

    easyvolcap: make imports easier to use

--- easyvolcap/dataloaders/__init__.py
+++ easyvolcap/dataloaders/__init__.py
@@ -5,7 +5,9 @@ for module in __all__:
     try:
         # from . import *  # the actual imports
         exec(f'from . import {module}')
+    except ModuleNotFoundError as e:
+        from easyvolcap.utils.console_utils import *
+        log(yellow(f'Failed to import {red(module)}{red(".py")}, missing package: {red(e.name)}'))
     except Exception as e:
         from easyvolcap.utils.console_utils import *
-        log(yellow(f'Failed to import submodule {module} of {__file__}'))
         stacktrace()  # print a full stacktrace for the users convenience
